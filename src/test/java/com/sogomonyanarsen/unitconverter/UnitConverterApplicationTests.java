67501676ee12b988d32c42ebf3c28a6383888ea8
Arsen Sogomonyan <sogomonyanarsen06@gmail.com>
2026-08-27 21:29:35 +0400

    chore: Spring Boot project setup

---
+++ src/test/java/com/sogomonyanarsen/unitconverter/UnitConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogomonyanarsen.unitconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
